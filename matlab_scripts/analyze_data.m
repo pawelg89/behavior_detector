886d6b086aae5ad03e2bc48249687e48d3d8e72c
Pawel Gardzinski <30794662+pawelg89@users.noreply.github.com>
2018-08-15 12:06:52 +0200

    Commenting out not ready function
    + adding '*.txt' files to gitignore

--- matlab_scripts/analyze_data.m
+++ matlab_scripts/analyze_data.m
@@ -1,9 +1,9 @@
 clear;
 output = AnalyzeData('badania_dane\collected_data.txt');
 
-figure(2);
-for i = 1 : data_size
-    hist(collected_data.data(i,:));
-    hold on;
-end
+% figure(2);
+% for i = 1 : data_size
+%     hist(collected_data.data(i,:));
+%     hold on;
+% end
 
