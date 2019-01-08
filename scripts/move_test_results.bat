4685c7e19f0a436e1f1a4dc71d2f92161087670a
Pawel Gardzinski <30794662+pawelg89@users.noreply.github.com>
2019-01-08 20:20:08 +0100

    Update scripts to move logs
    add new script to easier move all files
    add output messages to check in command line

--- scripts/move_test_results.bat
+++ scripts/move_test_results.bat
@@ -1,16 +1,36 @@
 echo OFF
-echo Hello World
+echo.
+echo.
+echo MOVE SCRIPT CALLED! Some files may be missing from their original place.
 set dir_path=%1
 set log_file=%2
 set events_dir=%dir_path%\events
 shift
 shift
-echo %dir_path% %log_file% 
+
+rem Create proper directory structure
+echo -----------------------------------------------------------------------------------
+echo Making path: %dir_path%
 mkdir %dir_path%
+rem Copy configuration files
+echo -----------------------------------------------------------------------------------
+echo Copying parameters.txt, descriptors_list.txt, behavior_names_list.txt into selected directory.
 copy /y parameters.txt %dir_path%
 copy /y descriptors_list.txt %dir_path%
 copy /y behavior_names_list.txt %dir_path%
+rem Move log file 
+echo -----------------------------------------------------------------------------------
+echo Moving log file: '%log_file%' into selected directory.
 move %log_file% %dir_path%
+rem Move collected_data into appropriate directory. It contains all
+rem test data collected during runtime.
+echo -----------------------------------------------------------------------------------
+echo Moving 'collected_data.txt' into selected dir_path.
 move collected_data.txt %dir_path%
-rd /s /q %events_dir%
+rem Move dir with detected behaviors
+echo -----------------------------------------------------------------------------------
+echo events_dir: %events_dir%
+rem rd /s /q %events_dir%
 move events %events_dir%
+echo.
+echo.
---
+++ scripts/move_test_results_no_args.bat
@@ -0,0 +1,4 @@
+set temp_path=C:\Users\Gardzin\Documents\DOKTORAT\BehaviorDetector2\BehaviorDetector\_BADANIA\01_bend
+set dir_path=%temp_path%\method3_thr016_test
+set temp_logfile=log_2019_1_8.txt
+move_test_results.bat %dir_path% %temp_logfile%
